35f05b5aaafcc97c50079feb4c0b08091297f6f6
Clinton Blackmore <clinton.blackmore@gmail.com>
2016-08-17 14:04:33 -0600

    Does basic builds from menu

---
+++ Assets/Editor/AutomaticBuilds.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AutomaticBuilds 
+{
+    public static string[] scenesToIncludeInReleaseBuild()
+    {
+        return new string[] {
+            "Assets/Scenes/Scene1.unity",
+            "Assets/Scenes/Scene2.unity"
+        };
+    }
+
+    public static string outputPathName()
+    {
+        return "Builds/Game";
+    }
+
+    public static void DoBuild(string buildName, BuildTarget target)
+    {
+        string message = BuildPipeline.BuildPlayer(
+            scenesToIncludeInReleaseBuild(),
+            outputPathName(),
+            target,
+            BuildOptions.ShowBuiltPlayer | BuildOptions.AutoRunPlayer);
+
+        if (string.IsNullOrEmpty(message))
+        {
+            Debug.LogFormat("{0} build completed successfully.", buildName);
+        }
+        else
+        {
+            Debug.LogErrorFormat("{0} build had errors.:\n{1}", buildName, message); 
+        }
+    }
+
+    [MenuItem("Builds/Mac")]
+    public static void BuildForMac() 
+    {
+        DoBuild("Mac OS X Build", BuildTarget.StandaloneOSXUniversal);
+    }
+
+    [MenuItem("Builds/Android")]
+    public static void BuildForAndroid() 
+    {
+        DoBuild("Android Build", BuildTarget.Android);
+    }
+
+}
