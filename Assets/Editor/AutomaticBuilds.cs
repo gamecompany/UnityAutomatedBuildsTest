e5893083d3cded8b8269e1dd4f5ea7e431fcee61
Eduardo Adolfo Arroyo Lopez <eduardo.arroyo177@gmail.com>
2016-08-30 15:25:16 -0500

    Use of targetPlatform from command line
    The targetPlatform is now used from the command line and the code was
    cleaned and refactored.

--- Assets/Editor/AutomaticBuilds.cs
+++ Assets/Editor/AutomaticBuilds.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using System;
+using System.Collections.Generic;
 
 public class AutomaticBuilds 
 {
+	const string k_TargetPlatformParam = "-targetPlatform=";
+	const string k_ResultFilePathParam = "-resultFilePath=";
+	static int errorCodeBadPlatform = 3;
+	
     public static string[] scenesToIncludeInReleaseBuild()
     {
         return new string[] {
@@ -21,21 +27,59 @@ public class AutomaticBuilds
         return Application.productName;
     }
 
-    public static string outputPathAndFilename(string extension)
+	// Copy and paste reuse from PlatformRunnerConfiguration.GetTempPath() from the UnityTestTools
+	public static string outputExtension(BuildTarget buildTarget)
+	{
+		switch (buildTarget)
+		{
+		case BuildTarget.StandaloneWindows:
+		case BuildTarget.StandaloneWindows64:
+			return "exe";
+		case BuildTarget.StandaloneOSXIntel:
+			return "app";
+		case BuildTarget.Android:
+			return "apk";
+		default:
+			if (buildTarget.ToString() == "BlackBerry" || buildTarget.ToString() == "BB10")
+				return "bar";
+			return "";
+		}
+	}
+
+	public static string outputPathAndFilename(BuildTarget targetPlatform)
     {
-        return string.Format("{0}\\{1}.{2}", outputPathName(), outputFilename(), extension);
+		return string.Format("{0}\\{1}.{2}", outputPathName(), outputFilename(), outputExtension(targetPlatform));
     }
 
-    public static void DoBuild(string buildName, string extension, BuildTarget target)
+	// This is meant to be called from the command line to automatically build the project:
+	// ex. Unity -quit -batchmode -projectPath <PROJECT_PATH> -targetPlatform=<PLATFORM> -executeMethod AutomaticBuilds.Build
+	// The targetPlatform comes from the BuildTarget enum, if you do not provide it, it won't work
+	public static void Build()
+	{
+		if (GetTargetPlatform().HasValue)
+		{
+			BuildTarget buildTarget = GetTargetPlatform().Value;
+			DoBuild(buildTarget);
+		}
+		else
+		{
+			Debug.LogError("No target platform specified");
+			EditorApplication.Exit(errorCodeBadPlatform);
+		} 
+	}
+		
+    public static void DoBuild(BuildTarget targetPlatform)
     {
+		string buildName = targetPlatform.ToString();
+
         Debug.LogFormat("Started '{0}' at {1}", buildName, System.DateTime.Now);
 
         System.IO.Directory.CreateDirectory(outputPathName());
 
         string message = BuildPipeline.BuildPlayer(
             scenesToIncludeInReleaseBuild(),
-            outputPathAndFilename(extension),
-            target,
+			outputPathAndFilename(targetPlatform),
+            targetPlatform,
             BuildOptions.ShowBuiltPlayer | BuildOptions.AutoRunPlayer);
 
         if (string.IsNullOrEmpty(message))
@@ -49,16 +93,40 @@ public class AutomaticBuilds
         }
     }
 
+	// Copy and paste reuse from UnityTest.Batch.GetTargetPlatform()
+	private static BuildTarget ? GetTargetPlatform()
+	{
+		string platformString = null;
+		BuildTarget buildTarget;
+		foreach (var arg in Environment.GetCommandLineArgs())
+		{
+			if (arg.ToLower().StartsWith(k_TargetPlatformParam.ToLower()))
+			{
+				platformString = arg.Substring(k_ResultFilePathParam.Length);
+				break;
+			}
+		}
+		try
+		{
+			if (platformString == null) return null;
+			buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), platformString);
+		}
+		catch
+		{
+			return null;
+		}
+		return buildTarget;
+	}
+
     [MenuItem("Builds/Mac")]
     public static void BuildForMac() 
     {
-        DoBuild("Mac OS X Build", "app", BuildTarget.StandaloneOSXUniversal);
+        DoBuild(BuildTarget.StandaloneOSXUniversal);
     }
 
     [MenuItem("Builds/Android")]
     public static void BuildForAndroid() 
     {
-        DoBuild("Android Build", "apk", BuildTarget.Android);
+        DoBuild(BuildTarget.Android);
     }
-
 }
