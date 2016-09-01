7927ef7b7cd4dbbb1a445702d80250be186072fd
Clinton Blackmore <clinton.blackmore@gmail.com>
2016-09-01 13:37:32 -0600

    Removed failing Unit Tests

--- Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
+++ Assets/UnityTestTools/Examples/UnitTestExamples/Editor/SampleTests.cs
@@ -11,42 +11,6 @@ namespace UnityTest
     internal class SampleTests
     {
         [Test]
-        [Category("Failing Tests")]
-        public void ExceptionTest()
-        {
-            throw new Exception("Exception throwing test");
-        }
-
-        [Test]
-        [Ignore("Ignored test")]
-        public void IgnoredTest()
-        {
-            throw new Exception("Ignored this test");
-        }
-
-        [Test]
-        [MaxTime(100)]
-        [Category("Failing Tests")]
-        public void SlowTest()
-        {
-            Thread.Sleep(200);
-        }
-
-        [Test]
-        [Category("Failing Tests")]
-        public void FailingTest()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
-        [Category("Failing Tests")]
-        public void InconclusiveTest()
-        {
-            Assert.Inconclusive();
-        }
-
-        [Test]
         public void PassingTest()
         {
             Assert.Pass();
@@ -65,38 +29,10 @@ namespace UnityTest
         }
 
         [Test]
-        [Culture("pl-PL")]
-        public void CultureSpecificTest()
-        {
-        }
-
-        [Test]
         [ExpectedException(typeof(ArgumentException), ExpectedMessage = "expected message")]
         public void ExpectedExceptionTest()
         {
             throw new ArgumentException("expected message");
-        }
-
-        [Datapoint]
-        public double zero = 0;
-        [Datapoint]
-        public double positive = 1;
-        [Datapoint]
-        public double negative = -1;
-        [Datapoint]
-        public double max = double.MaxValue;
-        [Datapoint]
-        public double infinity = double.PositiveInfinity;
-
-        [Theory]
-        public void SquareRootDefinition(double num)
-        {
-            Assume.That(num >= 0.0 && num < double.MaxValue);
-
-            var sqrt = Math.Sqrt(num);
-
-            Assert.That(sqrt >= 0.0);
-            Assert.That(sqrt * sqrt, Is.EqualTo(num).Within(0.000001));
         }
     }
 }
